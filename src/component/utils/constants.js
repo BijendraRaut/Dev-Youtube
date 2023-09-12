a9dbca5734443653b8ee014c1c9119b85387faf8
Bijendra Raut <61984063+BijendraRaut@users.noreply.github.com>
2023-09-12 23:56:58 +0530

    refactor - code formatting

---
+++ src/component/utils/constants.js
@@ -0,0 +1,4 @@
+const API_KEY = "AIzaSyDUoeZG4Xgb83guFsj9aLLscE6QcBc0KhY";
+export const YOUTUBE_API_URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" +
+  API_KEY;
