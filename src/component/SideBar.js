598d89eae99b8bc65badb7ad5ed9a89e2c673484
Bijendra Raut <61984063+BijendraRaut@users.noreply.github.com>
2023-09-14 23:28:26 +0530

    refactor - added rotueing to home of sidebar

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
@@ -9,7 +10,9 @@ const SideBar = () => {
   return (
     <div className="m-3 p-4 shadow-lg">
       <ul>
-        <li>Home</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>Shorts</li>
         <li>Videos</li>
         <li>Live</li>
