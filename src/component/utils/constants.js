ba61d32d4706aee39bc52bb8f65883456fb7cd73
Bijendra Raut <61984063+BijendraRaut@users.noreply.github.com>
2023-09-22 23:49:57 +0530

    update - api added for search suggestion

--- src/component/utils/constants.js
+++ src/component/utils/constants.js
@@ -2,3 +2,6 @@ const API_KEY = "AIzaSyDUoeZG4Xgb83guFsj9aLLscE6QcBc0KhY";
 export const YOUTUBE_API_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
+
+export const YOUTUBE_SUGGESTIONS_API_ =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
