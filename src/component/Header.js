96cc0793ebb2589d1e0fdcdc8d5a4c09159818f1
Bijendra Raut <61984063+BijendraRaut@users.noreply.github.com>
2023-09-23 23:55:00 +0530

    feat - added the function of storing cache result in store

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { toggleMenu } from "./utils/appSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { YOUTUBE_SUGGESTIONS_API_ } from "./utils/constants";
+import { cacheResults } from "./utils/searchSlice";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestion, setShowSuggestion] = useState(false);
+  const searchCache = useSelector((store) => store.search);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestions(), 3000);
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 300);
     return () => {
       clearTimeout(timer);
     };
@@ -17,9 +28,13 @@ const Header = () => {
     const data = await fetch(YOUTUBE_SUGGESTIONS_API_ + searchQuery);
     const json = await data.json();
     setSuggestions(json[1]);
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+    console.log(json[1]);
   };
-
-  const dispatch = useDispatch();
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -49,20 +64,24 @@ const Header = () => {
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestion(true)}
+            onBlur={() => setShowSuggestion(false)}
           />
           <button className="border border-gray-400 rounded-r-full p-1 bg-gray-100">
             🔍
           </button>
         </div>
-        <div className="fixed shadow-lg border-gray-500 w-96 rounded-r-lg px-5 py-2 bg-white border border-gray-100">
-          <ul>
-            {suggestions.map((s) => (
-              <li key={s} className="py-2 shadow-sm hover:bg-gray-100">
-                {s}
-              </li>
-            ))}
-          </ul>
-        </div>
+        {showSuggestion && (
+          <div className="absolute shadow-lg border-gray-500 w-96 rounded-r-lg px-5 py-2 bg-white border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 shadow-sm hover:bg-gray-100">
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
 
       <div className="col-span-1">
