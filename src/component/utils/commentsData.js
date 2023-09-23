ae8fbab8ab534801b8ffd06a406c70974a0cbf53
Bijendra Raut <61984063+BijendraRaut@users.noreply.github.com>
2023-09-23 23:59:39 +0530

    update - added the comment data

---
+++ src/component/utils/commentsData.js
@@ -0,0 +1,79 @@
+export const commentsData = [
+  {
+    name: "Ayush Tiwari",
+    text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+    replies: [],
+  },
+  {
+    name: "Ayush Tiwari",
+    text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+    replies: [
+      {
+        name: "Ayush Tiwari",
+        text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Ayush Tiwari",
+    text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+    replies: [
+      {
+        name: "Ayush Tiwari",
+        text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+        replies: [
+          {
+            name: "Ayush Tiwari",
+            text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+            replies: [
+              {
+                name: "Ayush Tiwari",
+                text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+                replies: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Ayush Tiwari",
+        text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+        replies: [
+          {
+            name: "Ayush Tiwari",
+            text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Ayush Tiwari",
+    text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+    replies: [],
+  },
+  {
+    name: "Ayush Tiwari",
+    text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+    replies: [
+      {
+        name: "Ayush Tiwari",
+        text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+        replies: [
+          {
+            name: "Ayush Tiwari",
+            text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Ayush Tiwari",
+    text: "It's very helpful brother, keep making more videos on this topic cause we will get an idea how to tackle such question while interview, again greet job please continue this series.",
+    replies: [],
+  },
+];
